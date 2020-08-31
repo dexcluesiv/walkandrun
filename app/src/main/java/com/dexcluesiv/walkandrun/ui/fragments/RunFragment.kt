26b3c9d163151140a537305e64d0888bea66197d
Inder Bagga <inder@dexcluesiv.com>
2020-08-31 19:09:49 +0530

    Added Google MapView for tracking

--- app/src/main/java/com/dexcluesiv/walkandrun/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/dexcluesiv/walkandrun/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.dexcluesiv.walkandrun.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,11 +9,15 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.dexcluesiv.walkandrun.R
 import com.dexcluesiv.walkandrun.ui.viewmodel.MainViewModel
+import com.dexcluesiv.walkandrun.utils.Commons
+import com.dexcluesiv.walkandrun.utils.Constants.Companion.REQUEST_CODE_LOCATION_PERMISSION
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -22,5 +28,53 @@ class RunFragment : Fragment(R.layout.fragment_run) {
 
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+
+        requestPermissions()
     }
+
+    private fun requestPermissions(){
+
+        if(Commons.hasLocationPermissions(requireContext())){
+            return
+        }
+
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.Q){
+
+            EasyPermissions.requestPermissions(this,
+                "You are requested to enable Location Permission before running/walking.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }else{
+
+            EasyPermissions.requestPermissions(this,
+                "You are requested to enable Location Permission before running/walking.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+
+            AppSettingsDialog.Builder(this).build().show()
+
+        }else requestPermissions()
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
 }
---
+++ app/src/main/java/com/dexcluesiv/walkandrun/utils/Commons.kt
@@ -0,0 +1,27 @@
+package com.dexcluesiv.walkandrun.utils
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+
+object Commons {
+
+    public fun hasLocationPermissions(context: Context): Boolean{
+
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.Q){
+
+            return EasyPermissions.hasPermissions(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        }else{
+
+            return EasyPermissions.hasPermissions(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+
+    }
+}
